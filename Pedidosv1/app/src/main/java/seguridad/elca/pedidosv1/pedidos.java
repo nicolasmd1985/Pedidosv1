ac7c6b2f5e01dacd1202aaa0398ff12381da6213
nicolasmd1985 <nicolasmelectronic@gmail.com>
2016-07-11 21:18:57 -0300

    login+tecnico

---
+++ Pedidosv1/app/src/main/java/seguridad/elca/pedidosv1/pedidos.java
@@ -0,0 +1,210 @@
+package seguridad.elca.pedidosv1;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import android.widget.TextView;
+
+import android.widget.Toast;
+
+public class pedidos extends ActionBarActivity {
+
+    // Progress Dialog
+    private ProgressDialog pDialog;
+   // String nombre = getIntent().getStringExtra("nombre");
+
+    private TextView pasarNombre;
+   // private static final String tecnico = null;
+
+    // Creating JSON Parser object
+    JSONParser jParser = new JSONParser();
+
+    ArrayList<HashMap<String, String>> empresaList;
+
+
+    // url to get all products list
+    private static String url_all_empresas = "http://192.168.5.48:2122/nicolas/pedidostec/get_pedido.php";
+
+    // JSON Node names
+    private static final String TAG_SUCCESS = "success";
+    private static final String TAG_PRODUCTS = "aux_pedido";
+    private static final String TAG_ID = "idauxpedido";
+    private static final String TAG_NOMBRE = "Empresa";
+    private static final String TAG_IDSOPORTE = "Numero";
+    //private static final String TAG_SOPORTE = "idnumsoporte";
+
+    // products JSONArray
+    JSONArray products = null;
+
+    ListView lista;
+
+
+
+//****************ESTO ES PARA DEVOLVERSE*****************
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // TODO Auto-generated method stub
+        if (keyCode == event.KEYCODE_BACK) {
+            Intent i = new Intent(pedidos.this, Login.class);
+            startActivity(i);
+            //return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+
+    //*****************PRINCIPAL*********************
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pedidos);
+
+
+        pasarNombre = (TextView) findViewById(R.id.validar);
+        String nombre = getIntent().getStringExtra("nombre");
+        pasarNombre.setText(nombre);
+
+        // Hashmap para el ListView
+        empresaList = new ArrayList<HashMap<String, String>>();
+
+        // Cargar los productos en el Background Thread
+        new LoadAllProducts().execute();
+        lista = (ListView) findViewById(R.id.listAllProducts);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+    }//fin onCreate
+
+
+
+
+
+
+    class LoadAllProducts extends AsyncTask<String, String, String> {
+
+        /**
+         * Antes de empezar el background thread Show Progress Dialog
+         * */
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(pedidos.this);
+            pDialog.setMessage("Cargando Pedidos. Por favor espere...");
+            //pDialog.setMessage(nombre);
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(false);
+            pDialog.show();
+        }
+
+        /**
+         * obteniendo todos los productos
+         * */
+        protected String doInBackground(String... args) {
+            // Building Parameters
+            List params = new ArrayList();
+            String username = getIntent().getStringExtra("nombre");
+            params.add(new BasicNameValuePair("username", username));
+            // getting JSON string from URL
+           // JSONObject json = jParser.makeHttpRequest(url_all_empresas, "GET", params);
+
+            JSONObject json = jParser.makeHttpRequest(url_all_empresas, "POST",
+                    params);
+
+
+
+            // Check your log cat for JSON reponse
+            Log.d("All Products: ", json.toString());
+
+            try {
+                // Checking for SUCCESS TAG
+                int success = json.getInt(TAG_SUCCESS);
+
+                if (success == 1) {
+                    // products found
+                    // Getting Array of Products
+                    products = json.getJSONArray(TAG_PRODUCTS);
+
+                    // looping through All Products
+                    //Log.i("ramiro", "produtos.length" + products.length());
+                    for (int i = 0; i < products.length(); i++) {
+                        JSONObject c = products.getJSONObject(i);
+
+                        // Storing each json item in variable
+                        String id = c.getString(TAG_ID);
+                        String name = c.getString(TAG_NOMBRE);
+                        String idsop = c.getString(TAG_IDSOPORTE);
+                        // creating new HashMap
+                        HashMap map = new HashMap();
+
+                        // adding each child node to HashMap key => value
+                        map.put(TAG_ID, id);
+                        map.put(TAG_NOMBRE, name);
+                        map.put(TAG_IDSOPORTE, idsop);
+
+                        empresaList.add(map);
+                    }
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+
+
+
+        /**
+         * After completing background task Dismiss the progress dialog
+         * **/
+        protected void onPostExecute(String file_url) {
+            // dismiss the dialog after getting all products
+            pDialog.dismiss();
+            // updating UI from Background Thread
+            runOnUiThread(new Runnable() {
+                public void run() {
+                    /**
+                     * Updating parsed JSON data into ListView
+                     * */
+                    ListAdapter adapter = new SimpleAdapter(
+                            pedidos.this,
+                            empresaList,
+                            R.layout.single_post,
+                            new String[] {
+                                    TAG_ID,
+                                    TAG_NOMBRE,
+                                    TAG_IDSOPORTE,
+
+
+                            },
+                            new int[] {
+                                    R.id.single_post_tv_id,
+                                    R.id.single_post_tv_nombre,
+                                    R.id.single_post_tv_idsop,
+                            });
+                    // updating listview
+                    //setListAdapter(adapter);
+                    lista.setAdapter(adapter);
+                }
+            });
+        }
+    }
+}
